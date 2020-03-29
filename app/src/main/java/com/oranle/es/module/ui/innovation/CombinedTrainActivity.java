8282371254fb8ca64c2f020aeeb3a29ff3708dcc
oranle <xucunyang@outlook.com>
2020-03-29 19:06:08 +0800

    1. 更改app名称 2. 创新能力改为： 综合训练和热身训练 3. ui细节调整

---
+++ app/src/main/java/com/oranle/es/module/ui/innovation/CombinedTrainActivity.java
@@ -0,0 +1,67 @@
+package com.oranle.es.module.ui.innovation;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.oranle.es.R;
+import com.oranle.es.databinding.ActivityCombinedTrainBinding;
+import com.oranle.es.module.base.BaseActivity;
+
+import org.jetbrains.annotations.Nullable;
+
+public class CombinedTrainActivity extends BaseActivity<ActivityCombinedTrainBinding> {
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_combined_train;
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    public void onLanguage(View view) {
+        startActivity(new Intent(this, TopicActivity.class));
+    }
+
+    public void onDigital(View view) {
+        startActivity(new Intent(this, DigitalActivity.class));
+    }
+
+    public void onAnalogy(View view) {
+        startActivity(new Intent(this, DigitalActivity.class));
+    }
+
+    public void onDialectical(View view) {
+        startActivity(new Intent(this, DigitalActivity.class));
+    }
+
+    public void onCombinedTrain(View view) {
+        startActivity(new Intent(this, WarmActivity.class));
+    }
+
+    public void onStrain(View view) {
+        startActivity(new Intent(this, StrainActivity.class));
+    }
+
+    public void onReverse(View view) {
+        startActivity(new Intent(this, ReverseActivity.class));
+    }
+
+    public void onLogicThink(View view) {
+        startActivity(new Intent(this, LogicThinkActivity.class));
+    }
+
+    public void onLenovo(View view) {
+        startActivity(new Intent(this, LenovoActivity.class));
+    }
+
+    public void onDivergentThink(View view) {
+        startActivity(new Intent(this, DivergentThinkActivity.class));
+    }
+
+    public void onImage(View view) {
+        startActivity(new Intent(this, ImageActivity.class));
+    }
+}
--- app/src/main/java/com/oranle/es/module/ui/innovation/InnovationActivity.java
+++ app/src/main/java/com/oranle/es/module/ui/innovation/InnovationActivity.java
@@ -5,62 +5,22 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.oranle.es.R;
+import com.oranle.es.databinding.ActivityInnovationBinding;
 import com.oranle.es.module.base.BaseActivity;
 
-import org.jetbrains.annotations.Nullable;
+public class InnovationActivity extends BaseActivity<ActivityInnovationBinding> {
 
-public class InnovationActivity extends BaseActivity {
     @Override
     public int getLayoutId() {
         return R.layout.activity_innovation;
-    }
-
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    public void onLanguage(View view) {
-        startActivity(new Intent(this, TopicActivity.class));
-    }
-
-    public void onDigital(View view) {
-        startActivity(new Intent(this, DigitalActivity.class));
-    }
-
-    public void onAnalogy(View view) {
-        startActivity(new Intent(this, DigitalActivity.class));
-    }
-
-    public void onDialectical(View view) {
-        startActivity(new Intent(this, DigitalActivity.class));
     }
 
     public void onWarm(View view) {
         startActivity(new Intent(this, WarmActivity.class));
     }
 
-    public void onStrain(View view) {
-        startActivity(new Intent(this, StrainActivity.class));
+    public void onCombinedTrain(View view) {
+        startActivity(new Intent(this, CombinedTrainActivity.class));
     }
 
-    public void onReverse(View view) {
-        startActivity(new Intent(this, ReverseActivity.class));
-    }
-
-    public void onLogicThink(View view) {
-        startActivity(new Intent(this, LogicThinkActivity.class));
-    }
-
-    public void onLenovo(View view) {
-        startActivity(new Intent(this, LenovoActivity.class));
-    }
-
-    public void onDivergentThink(View view) {
-        startActivity(new Intent(this, DivergentThinkActivity.class));
-    }
-
-    public void onImage(View view) {
-        startActivity(new Intent(this, ImageActivity.class));
-    }
 }
